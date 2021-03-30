fd1766e3a169dcaa4bdd385b927b30c85f5c7f26
SPARTAN-501 <luke.yin@alumni.ubc.ca>
2021-03-30 08:03:03 +0000

    Finished up to exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,10 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Peter", last_name: "Parker", hourly_rate: 15)
+@store1.employees.create(first_name: "Gwen", last_name: "Stacy", hourly_rate: 20)
+
+@store2.employees.create(first_name: "Bucky", last_name: "Barnes", hourly_rate: 25)
+@store2.employees.create(first_name: "Daisy", last_name: "Johnson", hourly_rate: 20)
+@store2.employees.create(first_name: "Melinda", last_name: "May", hourly_rate: 60)
